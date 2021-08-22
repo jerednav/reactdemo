8bf591c51f03925fbfd1b053f4e3329ca8abecaa
Jered <jerednav@gmail.com>
2021-08-21 23:59:28 -0700

    able to close modal from cancel, confirm or backdrop

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,17 @@
-import from 'react'
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
 
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen ] = useState(false); 
+
   function deleteHandler() {
-    
+    setModalIsOpen(true); 
   }
 
+  function closedModalHandler(){
+    setModalIsOpen(false);
+  }
 
   return (
     <div className="card">
@@ -12,6 +19,9 @@ function Todo(props) {
       <div className="actions">
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
+
+      {modalIsOpen &&  <Modal onCancel={closedModalHandler} onConfirm={closedModalHandler}/>}
+      {modalIsOpen &&  <Backdrop onClick={closedModalHandler} />}
     </div>
   );
 }
